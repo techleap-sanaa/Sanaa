0ee9f6cfb550f4d1b894d2954447a13ef007cf78
kc-clintone <kayseeclintone@gmail.com>
2025-12-03 16:49:43 +0300

    Edited the landing page file - added a landingpage layout

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,11 @@
+import LandingLayout from "../components/layout/LandingLayout";
+
+function LandingPage() {
+  return (
+    <LandingLayout>
+      {/* TODO: Hey guys, add content here */}
+    </LandingLayout>
+  );
+}
+
+export default LandingPage;
