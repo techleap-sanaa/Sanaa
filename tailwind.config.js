c918b810a9524526592f30ace420259ff8477695
Mitch Nyayieka <joynyayieka@gmail.com>
2025-12-04 15:27:55 +0300

    feat: configure custom fonts in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'clash': ['Clash Display', 'sans-serif'],
+        'lato': ['Lato', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
