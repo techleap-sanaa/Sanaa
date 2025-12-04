e6c6e98bbb527383daa691f7eef87734455d7caf
Mitch Nyayieka <joynyayieka@gmail.com>
2025-12-04 16:45:35 +0300

    feat: import Hero section into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Hero from './components/hero'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-              
+    <Hero />   
     </>
   )
 }
