9dd5f5c4adda0c7c88ef3dbdc9ce77333debe9de
gumfoot <nestagithaka@gmail.com>
2025-12-05 17:07:53 +0300

    feat:add final cta to landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,14 @@
 import FindInspo from "../components/FindInspo";
 import Hero from "../components/Hero";
 import LandingLayout from "../components/layout/LandingLayout";
+import FinalCTA from "../components/FinalCTA";
 
 function LandingPage() {
   return (
     <LandingLayout>
       <Hero />
       <FindInspo />
-      {/* TODO: Hey guys, add content here. Go to LandingLayout.jsx to build your components and add them here. */}
+      <FinalCTA />
     </LandingLayout>
   );
 }
