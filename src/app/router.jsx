06a19077340f7c89496fdc4464f8829cf183630a
kc-clintone <kayseeclintone@gmail.com>
2025-12-08 01:41:46 +0300

    Added a folder app to organize application files

---
+++ src/app/router.jsx
@@ -0,0 +1,47 @@
+import { createBrowserRouter } from "react-router-dom";
+import LandingPage from "../pages/landing/LandingPage";
+import SignInPage from "../pages/auth/SignInPage";
+import SignUpPage from "../pages/auth/SignUpPage";
+import DashboardPage from "../pages/shared/DashboardPage";
+import ProtectedRoute from "../components/ProtectedRoute";
+import PublicRoute from "../components/PublicRoute";
+import NotFoundPage from "@/pages/shared/NotFoundPage";
+
+
+// --- TODO: Hello guys, here is where we will put all our pages/routes from now on
+// --- follow the structure/style below to add a route/page
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/sign-in/*",
+    element: (
+      <PublicRoute>
+        <SignInPage />
+      </PublicRoute>
+    ),
+  },
+  {
+    path: "/sign-up/*",
+    element: (
+      <PublicRoute>
+        <SignUpPage />
+      </PublicRoute>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <ProtectedRoute>
+        <DashboardPage />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
