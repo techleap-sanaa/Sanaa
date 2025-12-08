75b7d7efb527a90d5a7ef21f2cb9bb85003a4dc9
fka-kafka <kigenbrandon@gmail.com>
2025-12-08 12:59:07 +0300

    fix: fixed missing image imports

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -4,6 +4,7 @@ import LandingLayout from "@/components/layout/LandingLayout";
 import FinalCta from "@/components/FinalCta";
 import AboutSanaaCreate from "@/components/AboutSanaaCreate";
 import SanaaAccordion from "@/components/Accordion";
+import FindYourNiche from "@/components/FindYourNiche";
 
 function LandingPage() {
   return (
