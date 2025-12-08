66bf7d784a98fedea92999cb114d3d21a6126ba5
fka-kafka <kigenbrandon@gmail.com>
2025-12-08 11:46:25 +0300

    Revert "feat: added the Find Your Niche section to the Landing Page"
    This reverts commit 3af2ed6ff01e08cb54d911429edb5d7b49187188.

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -4,14 +4,14 @@ import LandingLayout from "@/components/layout/LandingLayout";
 import FinalCta from "@/components/FinalCta";
 import AboutSanaaCreate from "@/components/AboutSanaaCreate";
 import SanaaAccordion from "@/components/Accordion";
-import FindYourNiche from "@/components/FindYourNiche";
 
 function LandingPage() {
   return (
     <LandingLayout>
       <Hero />
-      <AboutSanaaCreate />
+      <AboutSanaaCreate/>
       <FindInspo />
+      <SanaaAccordion/>
       <FinalCta />
 
     </LandingLayout>
