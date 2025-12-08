09154652a4184744f033df9db0568e4c5a26cff6
Ivy-Joy <ivyjoyweda@gmail.com>
2025-12-08 11:19:04 +0300

    Mod: Added font links to index.css and imported accordion + aboutsanna components to landingpage

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -2,13 +2,18 @@ import FindInspo from "@/components/FindInspo";
 import Hero from "@/components/Hero";
 import LandingLayout from "@/components/layout/LandingLayout";
 import FinalCta from "@/components/FinalCta";
+import AboutSanaaCreate from "@/components/AboutSanaaCreate";
+import SanaaAccordion from "@/components/Accordion";
 
 function LandingPage() {
   return (
     <LandingLayout>
       <Hero />
+      <AboutSanaaCreate/>
       <FindInspo />
+      <SanaaAccordion/>
       <FinalCta />
+
     </LandingLayout>
   );
 }
