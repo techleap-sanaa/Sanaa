6d93daa8dfd49e15a07ee159d3b29f00a2dd4ccb
kc-clintone <kayseeclintone@gmail.com>
2025-12-10 10:10:34 +0300

    feat: Added styles to login and signup pages for the background images

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -2,16 +2,26 @@ import { SignIn } from "@clerk/clerk-react";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="max-w-4xl w-3/5 rounded-2xl shadow-md flex justify-between p-2 gap-3">
-        <div className="w-3/6 py-36 px-6 rounded-2xl bg-[#E10600] align-middle">
-          <div className="text-3xl text-white font-bold text-left">
-            Welcome back
-          </div>
-          <h2 className="mt-2 text-white font-light text-[18px] max-w-[20rem] text-left">
-            Log in to connect with your creative circle, and discover new
-            inspiration
-          </h2>
+    <div
+      className="min-h-screen flex items-center justify-center bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/authImages/login.png')",
+      }}
+    >
+      <div className="max-w-4xl w-3/5 rounded-2xl shadow-md flex justify-between p-2 gap-3 bg-white">
+        <div
+          className="w-3/6 py-36 px-6 rounded-2xl align-middle bg-center bg-cover"
+          style={{
+            backgroundImage: "url('/authImages/login.png')",
+          }}
+        >
+            <div className="text-3xl text-white font-bold text-left">
+              Welcome back
+            </div>
+            <h2 className="mt-2 text-white font-light text-[18px] max-w-[20rem] text-left">
+              Log in to connect with your creative circle, and discover new
+              inspiration
+            </h2>
         </div>
         <div className="w-3/6">
           <SignIn routing="path" path="/sign-in" signUpUrl="/sign-up" />
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -2,13 +2,23 @@ import { SignUp } from "@clerk/clerk-react";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="max-w-4xl w-3/5 gap-5 rounded-2xl shadow-md flex p-3">
+    <div
+      className="min-h-screen flex items-center justify-center bg-cover bg-center"
+      style={{
+        backgroundImage: "url('/authImages/signup.png')",
+      }}
+    >
+      <div className="max-w-4xl w-3/5 gap-5 rounded-2xl shadow-md flex p-3 bg-white">
         <div className="w-3/6">
           <SignUp routing="path" path="/sign-up" signInUrl="/sign-in" />
         </div>
 
-        <div className="w-3/6 py-36 px-6 rounded-2xl bg-[#E10600] align-middle">
+        <div
+          className="w-3/6 py-36 px-6 rounded-2xl align-middle bg-center bg-cover"
+          style={{
+            backgroundImage: "url('/authImages/signup.png')",
+          }}
+        >
           <div className="text-3xl text-white font-bold text-left">
             Let's get started
           </div>
