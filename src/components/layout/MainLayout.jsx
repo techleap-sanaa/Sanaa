b1c015978020b834b1db22a885dffa2694c93f0b
gumfoot <nestagithaka@gmail.com>
2025-12-18 16:49:49 +0300

    mod: Modified mainlayout for proper responsiveness

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -11,7 +11,7 @@ export default function MainLayout({ children, asideContent }) {
         <div className="flex flex-1 flex-col">
           <AppHeader />
           <div className="flex flex-1 overflow-hidden">
-            <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
+            <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8 mt-[-30px]">
               {children}
             </main>
             {asideContent && <AppAside>{asideContent}</AppAside>}
